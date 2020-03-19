81a8db673b3165d1ed9fe3ba9601edc0d75260a2
maciejtrybilo <maciej@gini.net>
2020-03-19 11:58:27 +0100

    Fix tests.

--- GiniVisionExample/GiniVisionExampleUnitTests/AppCoordinatorTests.swift
+++ GiniVisionExample/GiniVisionExampleUnitTests/AppCoordinatorTests.swift
@@ -84,9 +84,11 @@ final class AppCoordinatorTests: XCTestCase {
         let url = urlFromImage(named: "invoice", fileExtension: "jpg")!
         appCoordinator.processExternalDocument(withUrl: url, sourceApplication: "testTarget")
         
-        let screenAPICoordinator = appCoordinator.childCoordinators.compactMap { $0 as? ScreenAPICoordinator }.first
-        XCTAssertNotNil(screenAPICoordinator, "screenAPICoordinator should not be nil after import file")
-        
+        wait(interval: 0.5) {
+            
+            let screenAPICoordinator = self.appCoordinator.childCoordinators.compactMap { $0 as? ScreenAPICoordinator }.first
+            XCTAssertNotNil(screenAPICoordinator, "screenAPICoordinator should not be nil after import file")
+        }
     }
     
     func testHelpAnimationWhenShowHelp() {
