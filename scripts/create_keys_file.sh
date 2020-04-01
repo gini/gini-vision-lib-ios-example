1824e1e682941592ef0902d4e484438b8a3f025a
maciejtrybilo <maciej@gini.net>
2020-04-01 09:51:37 +0200

    Added client_domain to Keys.plist

--- scripts/create_keys_file.sh
+++ scripts/create_keys_file.sh
@@ -18,6 +18,8 @@ printf '<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
+	<key>client_domain</key>
+	<string>gini.net</string>
 	<key>client_password</key>
 	<string>'$client_password'</string>
 	<key>'client_id'</key>
