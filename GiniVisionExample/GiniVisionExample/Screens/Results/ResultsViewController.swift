1712ba4ea6f7bce8559abf6ef9093cbea619ae13
Nadya Karaban <nadya@gini.net>
2021-06-11 14:07:46 +0200

    fixed xcode warnings in example (PIA-1554)

--- GiniVisionExample/GiniVisionExample/Screens/Results/ResultsViewController.swift
+++ GiniVisionExample/GiniVisionExample/Screens/Results/ResultsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-protocol ResultsViewControllerDelegate: class {
+protocol ResultsViewControllerDelegate: AnyObject {
     func results(viewController: ResultsViewController, didTapDone: ())
 }
 
